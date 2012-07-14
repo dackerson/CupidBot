87149fefa661d117199349a5e673217deb3a8c3e
Yassaman ZandMoghaddam <yassamanzm@gmail.com>
2012-07-14 16:25:16 -0300

    finally! the match maker class

--- match_maker.rb
+++ match_maker.rb
@@ -5,6 +5,12 @@ class MatchMaker
 	end
 
 	def add(name, location, gender)
+
+		if locations.has_key?(location)
+			locations[location]= [{:name=>name, :gender=>gender}]
+		else
+			locations[location] << {:name=>name, :gender=>gender}
+		end
 	end
 
 end
